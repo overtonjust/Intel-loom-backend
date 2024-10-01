c8cf7b0b7c2f2f7066a1dabb478f5320e0d56305
Marco Quispe <mquispe@pursuit.org>
2024-10-01 17:47:50 -0400

    Rebase from dev

---
+++ db/s3Config.js
@@ -0,0 +1,17 @@
+const {S3Client} = require('@aws-sdk/client-s3');
+const multer = require('multer');
+require('dotenv').config();
+
+const s3 = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
+
+const storage = multer.memoryStorage();
+
+const upload = multer({ storage: storage });
+
+module.exports = { upload, s3 };
