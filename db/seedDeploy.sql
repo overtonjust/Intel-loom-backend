efb6917063d85cb5c5b8e409f994e2ba1daf01ee
overtonjust <joverton@pursuit.org>
2024-11-04 08:23:05 -0500

    updated review content

--- db/seedDeploy.sql
+++ db/seedDeploy.sql
@@ -325,7 +325,7 @@ VALUES
 (4, 2, 'The class was structured well and easy to follow.'),
 (4, 3, 'Amazing teaching style, would take another class with them.'),
 (4, 5, 'Incredible class, very informative and interactive.'),
-(6, 1, 'Loved that fundamentals session! John was really helpful and even stayed a bit after to answer some questions I had. Hes becoming a true mentor to me. 5 stars!'),
+(6, 1, 'John was really helpful and even stayed a bit after to answer some questions I had. Hes becoming a true mentor to me!'),
 (6, 4, 'John is a natural code coach!'),
 (6, 5, 'Excellent instructor, I really learned a lot!'),
 (6, 2, 'Well-prepared and professional.');
